ceb6adffad534ca60f55c173c303ead5310ce399
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-12-27 12:48:30 +0300

    feat(playground): add constants and variables section
    * Introduced constants and variables explanation in Swift playground
    * Added examples for defining constants and variables with initial values

--- 1-TheBasics.playground/Contents.swift
+++ 1-TheBasics.playground/Contents.swift
@@ -9,3 +9,35 @@ import UIKit
  * Initialize edilmemiş bellek alanıyla ya da Initialize edilmemiş nesnelerle çalışmanızı engeller.
  * Swift, güvenlik kontrollerinin çoğunu kodunuzu oluştururken gerçekleştirir ve bazı durumlarda kodunuz çalışırken ek kontroller yapar.
  
+*/
+
+// MARK: Sabitler ve Değişkenler
+/*
+ * Sabitler ve değişkenler, bir adı (örneğin maximumNumberOfLoginAttempts veya welcomeMessage) belirli bir türdeki bir değerle (örneğin 10 sayısı veya "Hello" dizesi) ilişkilendirir.
+ * Sabitin değeri bir kez ayarlandıktan sonra değiştirilemez, ancak değişken ileride farklı bir değere ayarlanabilir.
+*/
+
+//MARK: Değişken ve Sabit Tanımlama
+
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
+
+
+//Kodunuzda depolanan bir değer değişmeyecekse, her zaman let anahtar sözcüğüyle sabit olarak tanımlayın. Değişen değerleri depolamak için yalnızca değişkenleri kullanın.
+
+//Bir sabit veya değişken tanımladığınızda, yukarıdaki örneklerde olduğu gibi, bu tanımlamanın bir parçası olarak ona bir değer verebilirsiniz. Alternatif olarak, ilk kez okuduğunuzda bir değere sahip olduğu garanti edildiği sürece, programın ilerleyen kısımlarında başlangıç değerini atayabilirsiniz.
+
+var environment = "development"
+let maximumNumberOfLoginAttempts2: Int
+// maximumNumberOfLoginAttempts2 henüz bir değeri yok
+
+
+if environment == "development" {
+    maximumNumberOfLoginAttempts2 = 100
+} else {
+    maximumNumberOfLoginAttempts2 = 10
+}
+// Now maximumNumberOfLoginAttempts bir değere sahip ve okunabilir.
+
+//Tek bir satırda virgülle ayrılmış birden fazla sabit veya değişken tanımlayabilirsiniz:
+var x = 0.0, y = 0.0, z = 0.0
