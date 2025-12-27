f15d25671c3cda2f469ec0baf8a2165ee73f0e06
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-12-27 11:37:37 +0300

    feat(playground): add initial Swift basics playground
    * Added a new playground with Swift basics explanations
    * Included essential concepts like constants, tuples, and optionals

---
+++ 1-TheBasics.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+// MARK: THE BASICS
+/*
+ * Swift değişmeyecek olan veriler için constant(let) kullanılmasını istiyor.
+ * Tuple'lar veri grupları oluşturmanıza ve bunları aktarmanıza olanak tanır. Bir tuple kullanarak birçok veriyi tek bir bileşik veri olarak gönderebilirsiniz
+ * Swift optional'ları kullanarak bir verinin yokluğunu işler. Optional türler kodun bir veriyi kullanmadan önce her zaman veri eksikliğini(null kontrolünü) kontrol etmesini sağlar.
+ * Type safety'i sağlayan bir dildir. Type safety kodunuzun çalıştığı Değer Türleri konusunda net olmanızı sağlar.
+ * Initialize edilmemiş bellek alanıyla ya da Initialize edilmemiş nesnelerle çalışmanızı engeller.
+ * Swift, güvenlik kontrollerinin çoğunu kodunuzu oluştururken gerçekleştirir ve bazı durumlarda kodunuz çalışırken ek kontroller yapar.
+ 
+ */
