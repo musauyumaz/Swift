c9cbbde272c281e5c532efbe3bbf2ce40a44460e
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2026-01-10 21:17:37 +0300

    feat(playground): add explanation for assignment operator in Swift
    * Added comments explaining the assignment operator functionality
    * Included example of tuple assignment and its behavior in Swift

--- 2-BasicOperators.playground/Contents.swift
+++ 2-BasicOperators.playground/Contents.swift
@@ -13,3 +13,21 @@ import UIKit
 //İkili operatörler iki hedef üzerinde işlem yapar (örneğin 2 + 3) ve iki hedef arasında göründükleri için infiks operatörlerdir.
 //Üçlü operatörler üç hedef üzerinde işlem yapar. C gibi, Swift'te de tek bir üçlü operatör vardır: üçlü koşullu operatör (a ? b : c).
 //Operatörlerin etkilediği değerler, işlenenlerdir. 1 + 2 ifadesinde, + sembolü bir infiks operatördür ve iki işleneni 1 ve 2 değerleridir.
+
+//MARK: Assignment Operator(Atama Operatörü)
+//Atama operatörü (a = b), a değerini b değeriyle başlatır veya günceller:
+
+let b = 10
+var a = 5
+a = b
+// a artık 10'a eşittir
+
+//Atamanın sağ tarafı birden çok değere sahip bir tuple ise, öğeleri birden çok sabit veya değişkene aynı anda ayrıştırılabilir:
+let (x, y) = (1, 2)
+// x 1'e eşittir ve y 2'ye eşittir
+//C ve Objective-C'deki atama operatörünün aksine, Swift'teki atama operatörü kendisi bir değer döndürmez. Aşağıdaki ifade geçerli değildir:
+
+//if x = y {
+    // Bu geçerli değildir, çünkü x = y bir değer döndürmez.
+//}
+//Bu özellik, eşittir operatörü (==) kullanılması gerektiğinde atama operatörünün (=) yanlışlıkla kullanılmasını önler. Swift, if x = y ifadesini geçersiz kılarak kodunuzda bu tür hataları önlemenize yardımcı olur.
