5e4098cf3a5366503189bb635d0914e429dd4d2a
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2026-01-16 22:22:56 +0300

    feat(strings): add initial strings and characters playground
    * Created a new playground for exploring strings and characters in Swift.
    * Added documentation on Swift's String and Character types.

---
+++ 3-StringsAndCharacters.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+//MARK: STRINGS AND CHARACTERS
+//Dize, "hello, world" veya "albatross" gibi bir dizi karakterdir. Swift dizeleri String türü ile temsil edilir. String içeriğine, Character değerlerinin bir koleksiyonu da dahil olmak üzere çeşitli şekillerde erişilebilir.
+
+//Swift'in String ve Character türleri, kodunuzda metinle çalışmak için hızlı, Unicode uyumlu bir yol sağlar. Dize oluşturma ve işleme sözdizimi, C'ye benzer bir dize sabit sözdizimi ile hafif ve okunaklıdır. Dize birleştirme, iki dizeyi + operatörü ile birleştirmek kadar basittir ve dize değiştirilebilirliği, Swift'teki diğer değerler gibi sabit veya değişken arasında seçim yapılarak yönetilir. Dizeleri, sabitleri, değişkenleri, literal'ları ve ifadeleri daha uzun dizelere eklemek için de kullanabilirsiniz. Bu işlem, dize enterpolasyonu olarak bilinir. Bu, görüntüleme, depolama ve yazdırma için özel dize değerleri oluşturmayı kolaylaştırır.
+
+//Sözdiziminin bu basitliğine rağmen, Swift'in String türü hızlı ve modern bir dize uygulamasıdır. Her dize, kodlamadan bağımsız Unicode karakterlerinden oluşur ve çeşitli Unicode temsilinde bu karakterlere erişim için destek sağlar.
+
+//NOTE: Swift'in String türü, Foundation'ın NSString sınıfıyla köprülenmiştir. Foundation ayrıca String'i genişleterek NSString tarafından tanımlanan yöntemleri ortaya çıkarır. Bu, Foundation'ı içe aktarırsanız, String üzerinde NSString yöntemlerine dönüştürme yapmadan erişebileceğiniz anlamına gelir.
